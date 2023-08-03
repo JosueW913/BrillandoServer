c831c3a0645072bccefe687e574fcb81d1ad1025
josueW913 <josuew913@gmail.com>
2023-08-03 11:16:44 -0400

    server initial setup

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var mongoose = require('mongoose')
+var cors = require('cors')
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -14,7 +17,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.set('trust proxy', 1);
+app.enable('trust proxy');
+
+app.use(
+    cors({
+      origin: [process.env.REACT_APP_URI]  // <== URL of our future React app
+    })
+  );
+// app.use(
+//     cors()
+//   );
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then((x) => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
 
 module.exports = app;
