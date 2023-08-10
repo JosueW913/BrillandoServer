5ebb23253024450e7708b465f340502bf5290a67
josueW913 <josuew913@gmail.com>
2023-08-10 15:37:36 -0400

    deployment

--- app.js
+++ app.js
@@ -24,14 +24,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('trust proxy', 1);
 app.enable('trust proxy');
 
-// app.use(
-//     cors({
-//       origin: [process.env.REACT_APP_URI]  // <== URL of our future React app
-//     })
-//   );
 app.use(
-    cors()
+    cors({
+      origin: [process.env.REACT_APP_URI]  // <== URL of our future React app
+    })
   );
+// app.use(
+//     cors()
+//   );
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
